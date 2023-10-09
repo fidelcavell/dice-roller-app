c8d8cea7fe0362872f9392ac65dc40c09e1039fd
VellxF <fidel.cavell@gmail.com>
2023-10-09 13:55:12 +0700

    Initial Commit

---
+++ lib/dice_roller.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentActiveDice = 1;
+
+  void rollDice() {
+    //Randomly change Dice Image on the screen
+    setState(() {
+      currentActiveDice = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$currentActiveDice.png',
+          width: 200,
+        ),
+        const SizedBox(height: 20), // used for padding
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text('Roll Dice'),
+        ),
+      ],
+    );
+  }
+}
